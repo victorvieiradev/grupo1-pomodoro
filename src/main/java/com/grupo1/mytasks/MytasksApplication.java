f8c13624a3676a2f2914704c6e6eec3e7b451153
victorvieiradev <contatovictorvieira@outlook.com>
2022-10-17 15:55:24 -0300

    start project

---
+++ src/main/java/com/grupo1/mytasks/MytasksApplication.java
@@ -0,0 +1,13 @@
+package com.grupo1.mytasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MytasksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo1/mytasks/MytasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo1.mytasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
