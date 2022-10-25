36d705f8dba57720762f1a684c3bc7bb18e0eca9
p3rsidh <persidh.pimenta@gmail.com>
2022-10-25 14:25:20 -0300

    Atualização de login

--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -2,25 +2,33 @@ const email = document.querySelector('#email')
 const senha = document.querySelector('#senha')
 var token
 
+
+
+
+
  btnEntrar.onclick = async e  =>{
+    e.preventDefault();
 
     if (email.value == '' || senha.value == '') {
       return
     }
-    let data = {"email": email.value, "senha":senha.value}
+    let paylog = {"email": email.value, "senha":senha.value}
   
     const response = await fetch('http://localhost:8080/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(data),
-    }),
-  
-    token = (response)
-    // if(response == )
-    localStorage.setItem("token", token)
+      body: JSON.stringify(paylog),
+    });
+    const data = await JSON.stringify(response)
     console.log(response)
- 
+
+    // if(response.status == 200){
+    //     // window.location.href = "tarefas.html";
+    //     localStorage.setItem("token", response.token)
+        
+    // }
+
   
   }
