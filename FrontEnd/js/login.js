36aba2ddb9f6844d0a81177d15a188cf236e5a03
victorvieiradev <contatovictorvieira@outlook.com>
2022-10-26 11:35:19 -0300

    token agora vai java script

--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -1,35 +1,32 @@
 const email = document.querySelector('#email')
 const senha = document.querySelector('#senha')
-
+const btnEntrar = document.querySelector('#btnEntrar')
 var token
 
-
-
-
- btnEntrar.onclick = async ()  =>{
-
-
-    let paylog = {"email": email.value, "senha":senha.value}
+btnEntrar.onclick =  e => {
   
-    const response = await fetch('http://localhost:8080/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(paylog),
-    })
-   
-    const reader = response.text
-    const result = await reader.read()
-    console.log(result.value)
-
-    // if(response.status == 200){
-    //     window.location.href = "tarefas.html";
-    //     localStorage.setItem("token", response.token)
-    //     console.log(response.token.JSON)
-        
-        
-    // }
-
-    
+  if (email.value == '' || senha.value == '') {
+    return
   }
+  let data = {"email": email.value, "senha":senha.value}
+
+fetch('http://localhost:8080/login', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(data),
+}).then( (response) => response.text().then( (data) => alert("Tarefa cadastrada com sucesso: " + JSON.stringify(data))))
+e.preventDefault();
+// window.location.href = "login.html";
+
+
+
+}
+
+
+
+
+
+
+ 
