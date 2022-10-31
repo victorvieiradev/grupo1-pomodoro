bbcb5e2f9d31386da6991688feda8c5a5badcdf7
p3rsidh <persidh.pimenta@gmail.com>
2022-10-31 10:11:49 -0300

    alteração de logout

--- FrontEnd/js/tarefas.js
+++ FrontEnd/js/tarefas.js
@@ -244,8 +244,13 @@ function insertItem(item, index) {
   tbody.appendChild(tr)
 }
 
+
+
+//FAZER FUNÇÃO GET COM DATABASE PARA MOSTRAR NA TELA
+
 const getItensBD = () => JSON.parse(localStorage.getItem('dbfunc')) ?? []
 const setItensBD = () => localStorage.setItem('dbfunc', JSON.stringify(itens))
+
 
 loadItens()
 
